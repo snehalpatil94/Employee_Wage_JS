4ca0d067aed79888a35780506ee4f23fbe206625
snehalpatil94 <erpatilsnehal@gmail.com>
2022-05-25 11:35:36 +0530

    Program to check employee present or not

---
+++ EmpWageComputation.js
@@ -0,0 +1,11 @@
+console.log("Welcome to Employee Wage Calculation \n");
+//UC1
+const IS_ABSENT = 0
+
+let empCheck = Math.floor(Math.random()*10) % 2;
+if(empCheck == IS_ABSENT){
+    console.log("Employee is absent");
+}
+else{
+    console.log("Employee is present");
+}
